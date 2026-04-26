40fce81c13fb5b93998242be06d869b7894d31b2
Edgar <edgarcastellanoniebles@gmail.com>
2026-04-26 18:27:58 -0500

    Update start.sh

--- REDES-master/REDES-master/start.sh
+++ REDES-master/REDES-master/start.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-cat > /app/.env << EOF
-APP_NAME=Redes
-APP_ENV=local
-APP_DEBUG=true
-APP_KEY=
-DB_CONNECTION=mysql
-DB_HOST=$DB_HOST
-DB_PORT=$DB_PORT
-DB_DATABASE=$DB_DATABASE
-DB_USERNAME=$DB_USERNAME
-DB_PASSWORD=$DB_PASSWORD
-SESSION_DRIVER=cookie
-CACHE_DRIVER=array
-EOF
+echo "APP_NAME=Redes" > /app/.env
+echo "APP_ENV=local" >> /app/.env
+echo "APP_DEBUG=true" >> /app/.env
+echo "APP_KEY=" >> /app/.env
+echo "DB_CONNECTION=mysql" >> /app/.env
+echo "DB_HOST=${DB_HOST}" >> /app/.env
+echo "DB_PORT=${DB_PORT}" >> /app/.env
+echo "DB_DATABASE=${DB_DATABASE}" >> /app/.env
+echo "DB_USERNAME=${DB_USERNAME}" >> /app/.env
+echo "DB_PASSWORD=${DB_PASSWORD}" >> /app/.env
+echo "SESSION_DRIVER=cookie" >> /app/.env
+echo "CACHE_DRIVER=array" >> /app/.env
 
 php artisan key:generate
 php artisan config:clear
